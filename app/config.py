f953390e6f8914cf82519c42e4843c7a24e8ced8
Angelica <Angelica137@users.noreply.github.com>
2020-08-12 16:59:19 +0100

    add blog entry view

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ import os
 class Configuration(object):
     APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/blog.db' % APPLICATION_DIR
+    SQLALCHEMY_DATABASE_URI = 'postgresql:///blog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
