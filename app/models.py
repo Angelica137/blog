95f6ba2ee7066dcca1f87716e0a3a69840237669
Angelica <Angelica137@users.noreply.github.com>
2020-08-12 13:55:20 +0100

    complete tag-entry table

--- app/models.py
+++ app/models.py
@@ -1,8 +1,12 @@
 import datetime, re
 from app import db
 
+
 def slugify(s):
     return re.sub('[^\w]+', '-', s).lower()
+
+
+entry_tags = db.Table('entry_tags', db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')), db.Column('entry_id', db.Integer, db.ForeignKey('entry.id')))
 
 class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,6 +18,8 @@ class Entry(db.Model):
         db.DateTime,
         default=datetime.datetime.now, 
         onupdate=datetime.datetime.now)
+
+    tags = db.relationship('Tag', secondary=entry_tags, backref=db.backref('entries', lazy='dynamic'))
 
     def __init__(self, *args, **kwargs):
         super(Entry, self).__init__(*args, **kwargs)  # Call parent constructor.
@@ -34,7 +40,7 @@ class Tag(db.Model):
     slug = db.Column(db.String(64), unique=True)
 
     def __init__(self, *args, **kwargs):
-        super(Tag, self).__init__.(*args, **kwargs)
+        super(Tag, self).__init__(*args, **kwargs)
         self.slug = slugify(self.name)
 
     def __repr__(self):
