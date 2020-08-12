69560af8712a5bc3ac6402d33453e350b83eb382
Angelica <Angelica137@users.noreply.github.com>
2020-08-12 15:22:30 +0100

    add entries blue print

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from app import app, db
+import models
 import views
+
+
+from entries.blueprint import entries
+app.register_blueprint(entries, url_prefix='/entries')
 
 
 if __name__ == '__main__':
