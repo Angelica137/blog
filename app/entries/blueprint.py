0c388b65aabd258ec3c25287de81f7dcdcf20df5
Angelica <Angelica137@users.noreply.github.com>
2020-08-13 11:11:07 +0100

    add search to views

--- app/entries/blueprint.py
+++ app/entries/blueprint.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from helpers import object_list
 from models import Entry, Tag
+
+
+def entry_list(template, query, **context):
+    search = request.args.get('q')
+    if search:
+        query = query.filter((Entry.body.contains(search)) | (Entry.title.contains(search)))
+    return object_list(template, query, **context)
+
 
 
 entries = Blueprint('entries', __name__, template_folder='templates')
@@ -10,13 +18,13 @@ entries = Blueprint('entries', __name__, template_folder='templates')
 @entries.route('/')
 def index():
     entries = Entry.query.order_by(Entry.created_timestamp.desc())
-    return object_list('entries/index.html', entries)
+    return entry_list('entries/index.html', entries)
 
 
 @entries.route('/tags/')
 def tag_index():
     tags = Tag.query.order_by(Tag.name)
-    return object_list('entries/tag_index.html', tags)
+    return entry_list('entries/tag_index.html', tags)
 
 
 @entries.route('/tags/<slug>')
