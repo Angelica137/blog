919a9e2608c38136af631cb0a06106c0952189fe
Angelica <Angelica137@users.noreply.github.com>
2020-08-24 18:46:44 +0100

    go abck to basics

--- app/entries/blueprint.py
+++ app/entries/blueprint.py
@@ -1,84 +1,23 @@
-from flask import Blueprint, redirect, render_template, request, url_for
+from flask import Blueprint
 
-from app import db
 from helpers import object_list
 from models import Entry, Tag
-from entries.forms import EntryForm
-
-
-def entry_list(template, query, **context):
-    valid_statuses = (Entry.STATUS_PUBLIC, Entry.STATUS_DRAFT)
-    query = query.filter(Entry.status.in_(valid_statuses))
-    if request.args.get('q'):
-        search = request.args['q']
-        query = query.filter((Entry.body.contains(search)) | (Entry.title.contains(search)))
-    return object_list(template, query, **context)
-
 
 entries = Blueprint('entries', __name__, template_folder='templates')
-
 
 @entries.route('/')
 def index():
     entries = Entry.query.order_by(Entry.created_timestamp.desc())
-    return entry_list('entries/index.html', entries)
-
+    return object_list('entries/index.html', entries)
 
 @entries.route('/tags/')
 def tag_index():
-    tags = Tag.query.order_by(Tag.name)
-    return entry_list('entries/tag_index.html', tags)
+    pass
 
-
-@entries.route('/tags/<slug>')
+@entries.route('/tags/<slug>/')
 def tag_detail(slug):
-    tag = Tag.query.filter(Tag.slug == slug).first_or_404()
-    entries = tag.entries.order_by(Entry.created_tomestam.desc())
-    return object_list('entries/tag_detail.html', entries, tag=tag)
+    pass
 
-
-@entries.route('/create/', methods=['GET', 'POST'])
-def create():
-    if request.method == 'POST':
-        form = EntryForm(request.form)
-        if form.validate():
-            entry = form.save_entry(Entry())
-            db.session.add(entry)
-            db.session.commit()
-            return redirect(url_for('entries.detail', slug=entry.slug))
-    else:
-        form = EntryForm()
-
-    return render_template('entries/create.html', form=form)
-
-
-@entries.route('/<slug>')
+@entries.route('/<slug>/')
 def detail(slug):
-    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
-    return render_template('entries/detail.html', entry=entry)
-
-
-@entries.route('/<slug>/edit/', methods=['GET', 'POST'])
-def edit(slug):
-    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
-    if request.method == 'POST':
-        form = EntryForm(request.form, obj=entry)
-        db.session.add(entry)
-        db.session.commit()
-        return redirect(url_for('entries.detail', slug=entry.slug))
-    else:
-        form = EntryForm(obj=entry)
-
-    return render_template('entries/edit.html', entry=entry, form=form)
-
-
-@entries.route('/<slug>/delete/', methods=['GET', 'POST'])
-def delete(slug):
-    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
-    if request.method == 'POST':
-        entry.status = Entry.STATUS_DELETED
-        db.session.add(entry)
-        db.session.commit()
-        return redirect(url_for('entries.index'))
-
-    return render_template('entries/delete.html', entry=entry)
+    pass
