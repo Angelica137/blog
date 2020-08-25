01c52edf2fc20135420072cdf5f11e24410b4c7d
Angelica <Angelica137@users.noreply.github.com>
2020-08-25 12:49:37 +0100

    fix typo in blueprint

--- app/entries/forms.py
+++ app/entries/forms.py
@@ -14,20 +14,25 @@ class TagField(wtforms.StringField):
 
     def get_tags_from_string(self, tag_string):
         raw_tags = tag_string.lower().split(',')
-
-        # Filter out any empty tag names.
+        '''
+        Filter out any empty tag names.
+        '''
         tag_names = [name.strip() for name in raw_tags if name.strip()]
-
-        # Query the database and retrieve any tags we have already saved.
+        '''
+        Query the database and retrieve any tags we have already saved.
+        '''
         existing_tags = Tag.query.filter(Tag.name.in_(tag_names))
-
-        # Determine which tag names are new.
+        '''
+        Determine which tag names are new.
+				'''
         new_names = set(tag_names) - set([tag.name for tag in existing_tags])
-
-        # Create a list of unsaved Tag instances for the new tags.
+        '''
+        Create a list of unsaved Tag instances for the new tags.
+				'''
         new_tags = [Tag(name=name) for name in new_names]
-
-        # Return all the existing tags + all the new, unsaved tags.
+        '''
+        Return all the existing tags + all the new, unsaved tags.
+				'''
         return list(existing_tags) + new_tags
 
     def process_formdata(self, valuelist):
