95ab568728a16c7948a0fa95c7095fe36cdb8f66
Angelica <Angelica137@users.noreply.github.com>
2020-08-25 12:37:53 +0100

    add tag field to form macro

--- app/entries/forms.py
+++ app/entries/forms.py
@@ -47,6 +47,7 @@ class EntryForm(wtforms.Form):
     title = wtforms.StringField('Title', validators=[DataRequired()])
     body = wtforms.TextAreaField('Body', validators=[DataRequired()])
     status = wtforms.SelectField('Entry status', choices=((Entry.STATUS_PUBLIC, 'Public'), (Entry.STATUS_DRAFT, 'Draft')), coerce=int)
+    tags = TagField('Tags', description='Separate multiple tags with commas.')
 
     def save_entry(self, entry):
         self.populate_obj(entry)
