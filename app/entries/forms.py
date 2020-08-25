1ebcfa568c7813edbae304830b70f089d815381a
Angelica <Angelica137@users.noreply.github.com>
2020-08-25 12:31:51 +0100

    add Tag form

--- app/entries/forms.py
+++ app/entries/forms.py
@@ -1,7 +1,47 @@
 import wtforms
 from wtforms.validators import DataRequired
 
-from models import Entry
+
+from models import Entry, Tag
+
+
+class TagField(wtforms.StringField):
+    def _value(self):
+        if self.data:
+            '''
+            Display data as a comma-separated list.
+            '''
+            return ', '.join([tag.name for tag in self.data])
+        return ''
+
+    def get_tags_from_string(self, tag_string):
+        raw_tags = tag_string.lower().split(',')
+        '''
+        Filter out any empty tag names.
+        '''
+        tag_names = [name.strip() for name in raw_tags if name.strip()]
+        '''
+        Query the database and retrieve any tags we have already daved.
+        '''
+        existin_tags = Tag.query.filter(Tag.name.in_(tag_names))
+        '''
+        Determine which tag names are new
+        '''
+        new_names = set(tag_names) - set([tag.name for tag in existing_tags])
+        '''
+        Create a list of unsaved Tag instances for the new tags.
+        '''
+        new_tags = [Tag(name=name) for name in new_names]
+        '''
+        Return all the existing tags + all new tags.
+        '''
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            self.data = self.get_tags_from_string(valuelist[0])
+        else:
+            self.data = []
+
 
 class EntryForm(wtforms.Form):
     title = wtforms.StringField('Title', validators=[DataRequired()])
